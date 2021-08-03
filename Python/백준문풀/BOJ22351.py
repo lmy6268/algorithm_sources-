dae4994d53ddd8f8f9809d7f57cd29b2436d9be7
lmy6268 <70847610+lmy6268@users.noreply.github.com>
2021-08-03 22:54:09 +0900

    못풀은 문제 나중에 돌아볼것.

--- Python/백준문풀/BOJ22351.py
+++ Python/백준문풀/BOJ22351.py
@@ -1,6 +1,33 @@
 #수학은 체육과목 입니다
 import sys;p=sys.stdin.readline
-s=p()
+s=p().rstrip()
+volume=1#배열의 칸수를 얼마나 차지하는지
+i=0
+result=[]
+
 while True:
-    i=0
+    length=(i+volume)#처음은 0+1=1/2+1=3/4  
+    k=int(s[i:length]) #1차: s[0:1] /2차:s[2:3] / 3: s[2:4]
+    if length>len(s)-1:
+        # if len(result)==0:
+        #     print("sda")
+        #     print(s,s)
+        # else:
+        # print(result[0],result[-1:])
+        break 
+    cp=int(s[i+1:length+1])
+    if cp==k+1:#s[1:2] 
+        i=length#i=2
+        result.append(k)#result=[s[0],]
+    if cp<k+1:#s[3:4]==1 <10
+        # i=length+1#i=2
+        print(cp)
+        volume+=1
+        result.append(k)
+    if cp==k:
+        volume+=1
+        
+
+print(result)
+    
     
