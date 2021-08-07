9ac40acb412bd548ff6a7ad6eecb444f56f20fb3
lmy6268 <70847610+lmy6268@users.noreply.github.com>
2021-08-07 14:52:31 +0900

    공부한 후 내가 코딩해보는 선택정렬 기본코딩

--- Python/이코테/정렬/선택/selectsort.py
+++ Python/이코테/정렬/선택/selectsort.py
@@ -1,11 +1,22 @@
-#오름차순 정렬 기준
-data=[7,5,9,0,3,1,6,2,4,8]
-#선택정렬 알고리즘 기본
-len_data=len(data)
-for i in range(len_data):
-    min_index=i #가장 작은 인덱스
-    for j in range(i+1,len_data):
-        if data[min_index]>data[j]: #만약 현재 데이터가 맨앞의 데이터보다 값이 작을 경우
-            min_index=j 
-    data[i], data[min_index] = data[min_index],data[i] #스왑
-print(data)    
+
+def selectSort(arr): #선택정렬
+    n=len(arr)
+    for i in range(n): #O(N)
+        min_index=i
+        #오름차순 정렬
+        for j in range(i+1,n): #O(N)
+            if arr[min_index]>arr[j]:
+                min_index=j
+        #내림차순 정렬
+        '''
+        for j in range(i+1,n):
+            if arr[min_index]<arr[j]:
+                min_index=j#범위 내에서 가장 큰 값을 계속해서 앞으로 끄집어냄 
+        '''
+        arr[i],arr[min_index]=arr[min_index],arr[i] #O(1)
+    #시간 복잡도 : O(N)*O(N)= O(N^2)
+    return arr
+#입력한 데이터 
+arr=[7,3,2,0,1,4,9,6,5,8]
+#정렬 실행
+print(selectSort(arr))
